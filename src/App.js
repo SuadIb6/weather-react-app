72a25247559c9bf2ba54cfbd4e6eff3f7e31ae75
SuadIb6 <suadibrahim1020@gmail.com>
2023-09-02 20:59:41 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Suad Ibrahim and is{" "}
-        <a href="https://github.com/SuadIb6/weather-react-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Suad Ibrahim and is{" "}
+          <a
+            href="https://github.com/SuadIb6/weather-react-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
