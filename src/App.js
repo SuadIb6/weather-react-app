280b0d781b61635f95b0b225e1aeb1d376242d78
SuadIb6 <suadibrahim1020@gmail.com>
2023-09-02 20:27:37 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Suad Ibrahim and is{" "}
+        <a href="https://github.com/SuadIb6/weather-react-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
