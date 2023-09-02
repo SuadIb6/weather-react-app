d4d351c54ade3ce263d8da09cf922e723bebbc1a
SuadIb6 <suadibrahim1020@gmail.com>
2023-09-02 23:31:26 +0100

    Added animated weather icons

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="London" />
         <footer>
           This project was coded by Suad Ibrahim and is{" "}
           <a
             href="https://github.com/SuadIb6/weather-react-app"
             target="_blank"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>
